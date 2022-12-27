2db800a8537ed9f553968d948805500baf626847
MoNeN-92 <ika.tatishvili@gmail.com>
2022-12-27 16:47:01 +0400

    restor

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {  Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MoviePageComponent } from './movie-page/movie-page.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+
+
+
+
+const routes: Routes = [
+{
+  path:'MoviePage/:id',
+  component:MoviePageComponent
+},
+{
+  path:'mainPage',
+  component:BodyComponent
+},
+{
+  path:'HeaderPage',
+  component:HeaderComponent
+},
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
