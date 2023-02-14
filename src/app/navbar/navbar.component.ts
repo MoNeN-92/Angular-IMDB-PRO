792003803eab52963206a87b858800bfc3135d0c
MoNeN-92 <ika.tatishvili@gmail.com>
2023-02-14 18:07:18 +0400

    main page responsive fixed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,7 +5,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
+
 export class NavbarComponent implements OnInit {
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    const child1 = document.querySelector('.dro-menu-child-1');
+    const child3 = document.querySelector('.dro-menu-child-3');
+    if (child1 && child3) {
+      if (this.isDropdownOpen) {
+        child1.classList.add('rotate');
+        child3.classList.add('rotate');
+      } else {
+        child1.classList.remove('rotate');
+        child3.classList.remove('rotate');
+      }
+    }
+  }
+  
+  
 
   constructor() { }
 
